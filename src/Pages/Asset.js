85af73f0e8de4e1bea1fa3a5f3c3d064e5602b88
Tcarr9442 <35206339+Tcarr9442@users.noreply.github.com>
2021-09-15 09:11:55 -0400

    Update Asset.js

--- src/Pages/Asset.js
+++ src/Pages/Asset.js
@@ -15,17 +15,31 @@ function AssetPage() {
         setDate(document.getElementById('date_selector').value)
     }
 
+    const handleTextInputChange = id => {
+        console.log('storing', id)
+        if (id === 'new') {
+            //data validation
+            let job_code = ''
+            //send to api
+        } else for (let i of data) {
+            if (id === i.id) {
+                //data validation
+                //send to api
+            }
+        }
+    }
+
     /**
      * Function to control rendering of data
      * 
      */
     function RenderRow(row) {
         //TO DO: Add ID or some sorting to split daily dollars from the rest
-        console.log(row)
+        console.log(row) //add events to the inputs for on unfocus or on change
         return (<tr>
-            <td><input type='text' value={row.job_code} className='jobcode'></input></td>
-            <td><input type='text' value={row.asset_id} className='jobcode'></input></td>
-            <td><input type='text' value={row.notes ? row.notes : ''} className='jobcode'></input></td>
+            <td><input type='text' value={row.job_code} className='jobcode' id={`${row.id}-jobcode`} onBlur={(e) => handleTextInputChange(row.id)}></input></td>
+            <td><input type='text' value={row.asset_id} className='asset_id' id={`${row.id}-assetid`} onBlur={(e) => handleTextInputChange(row.id)}></input></td>
+            <td><input type='text' value={row.notes ? row.notes : ''} className='notes' id={`${row.id}-notes`} onBlur={(e) => handleTextInputChange(row.id)}></input></td>
         </tr>)
     }
 
@@ -34,16 +48,22 @@ function AssetPage() {
     else return (
         <>
             <input type='date' className='date' id='date_selector' value={getDate(date)} onChange={handleDateChange} />
-            <form>
-                <table className='rows'>
-                    <tr>
-                        <th>Job Code</th>
-                        <th>Asset Tag</th>
-                        <th>Comments</th>
-                    </tr>
-                    {data.records.map(m => RenderRow(m))}
-                </table>
-            </form>
+            <table className='rows'>
+                <tr>
+                    <th>Job Code</th>
+                    <th>Asset Tag</th>
+                    <th>Comments</th>
+                </tr>
+                {data ? data.records.map(m => RenderRow(m)) : <></>}
+                <tr>
+                    <td>
+                        <input type='text' className='jobcode' id={`new-jobcode`} onBlur={(e) => handleTextInputChange('new')}></input>
+
+                    </td>
+                    <td><input type='text' className='asset_id' id={`new-assetid`} onBlur={(e) => handleTextInputChange('new')}></input></td>
+                    <td><input type='text' className='notes' id={`new-notes`} onBlur={(e) => handleTextInputChange('new')}></input></td>
+                </tr>
+            </table>
             <PageTemplate highLight='1' />
         </>
     )
