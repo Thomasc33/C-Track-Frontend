b0e56ae7179f3da38448b317fd83697910eff68e
Tcarr9442 <35206339+Tcarr9442@users.noreply.github.com>
2022-04-29 14:31:18 -0400

    New job code has a save button now

--- src/Pages/Jobs.js
+++ src/Pages/Jobs.js
@@ -22,7 +22,7 @@ function JobPage(props) {
     const [newPrice, setNewPrice] = useState(0);
     const [newIsHourly, setNewIsHourly] = useState(false)
     const [newIsAsset, setNewIsAsset] = useState(false)
-    const [newstatusOnly, setNewstatusOnly] = useState(false)
+    const [newStatusOnly, setNewStatusOnly] = useState(false)
     const [newAppliesSelection, setNewAppliesSelection] = useState([])
     const [newHourlyGoal, setNewHourlyGoal] = useState(null)
     const [updated, setUpdated] = useState(0)
@@ -102,90 +102,16 @@ function JobPage(props) {
             if (e.target.classList.contains('invalid')) e.target.classList.remove('invalid')
         }
         if (id === 'new') {
-            if (inApi) return
-            let job_code = newJobCode;
-            let job_name = newJobName;
-            let price = newPrice;
-            let isHourly = newIsHourly;
-            let isAsset = newIsAsset
-            let applies = newAppliesSelection;
-            let hourly_goal = newHourlyGoal;
-            let statusOnly = newstatusOnly;
-            let restricted_comments = newRestrictedComments;
-            if (e.isHourly) { isHourly = e.selection; setNewIsHourly(e.selection) }
-            else if (e.isAsset) { isAsset = e.selection; setNewIsAsset(e.selection) }
-            else if (e.isSelect) { applies = e.selection.map(m => m.value).join(',') }
-            else if (e.statusOnly) { statusOnly = e.selection; setNewstatusOnly(e.selection) }
+            if (e.isHourly) setNewIsHourly(e.selection)
+            else if (e.isAsset) setNewIsAsset(e.selection)
+            else if (e.statusOnly) setNewStatusOnly(e.selection)
             else switch (e.target.id) {
-                case 'new-price':
-                    price = e.target.value
-                    setNewPrice(e.target.value)
-                    break;
-                case 'new-hourly_goal':
-                    hourly_goal = e.target.value
-                    setNewHourlyGoal(e.target.value)
-                    break;
-                case 'new-jobname':
-                    job_name = e.target.value
-                    setNewJobName(e.target.value)
-                    break;
-                case 'new-jobcode':
-                    job_code = e.target.value
-                    setNewJobCode(e.target.value)
-                    break;
-                case 'new-comments':
-                    restricted_comments = e.target.value
-                    setNewRestrictedComments(e.target.value)
-                    break
-                default:
-                    console.log('Default Case hit for new')
-                    return
-            }
-
-            //data validation
-            let cont = true;
-            if (!job_code) {
-                document.getElementById('new-jobcode').classList.add('invalid')
-                cont = false
-            }
-            if (!job_name) {
-                document.getElementById('new-jobname').classList.add('invalid')
-                cont = false
-            }
-            if (!price) {
-                document.getElementById('new-price').classList.add('invalid')
-                cont = false
-            }
-            if (!cont) return
-
-            //send to api
-            let formData = { job_code, job_name, price, isHourly, applies, isAsset, hourly_goal, statusOnly, restricted_comments }
-            setLoading(true)
-            let token = await getTokenSilently()
-            let res = await jobService.add(formData, token)
-            if (res.isErrored) {
-                setLoading(false)
-                if (document.getElementById('new-jobcode')) document.getElementById('new-jobcode').classList.add('invalid')
-                if (document.getElementById('new-jobname')) document.getElementById('new-jobname').classList.add('invalid')
-                if (document.getElementById('new-price')) document.getElementById('new-price').classList.add('invalid')
-            } else {
-                if (document.getElementById('new-jobcode')) document.getElementById('new-jobcode').value = ''
-                if (document.getElementById('new-jobname')) document.getElementById('new-jobname').value = ''
-                if (document.getElementById('new-price')) document.getElementById('new-price').value = ''
-                setNewPrice(0)
-                setNewJobName('')
-                setNewJobCode('')
-                const response = await fetch(`${APILink}/all`, {
-                    mode: 'cors',
-                    headers: {
-                        'Authorization': `Bearer ${token}`,
-                        'Access-Control-Allow-Origin': '*',
-                        'X-Version': require('../backendVersion.json').version
-                    }
-                });
-                const d = await response.json();
-                setData(d);
-                setLoading(false)
+                case 'new-price': setNewPrice(e.target.value); break;
+                case 'new-hourly_goal': setNewHourlyGoal(e.target.value); break;
+                case 'new-jobname': setNewJobName(e.target.value); break;
+                case 'new-jobcode': setNewJobCode(e.target.value); break;
+                case 'new-comments': setNewRestrictedComments(e.target.value); break
+                default: console.log('Default Case hit for new in jobs'); return
             }
         } else for (let i of data.job_codes) {
             if (id === i.id) {
@@ -248,6 +174,47 @@ function JobPage(props) {
                     if (!e.isHourly && !e.isSelect && !e.isAsset) e.target.classList.add('invalid')
                 }
             }
+        }
+    }
+
+    const sendNewToAPI = async () => {
+        let formData = {
+            job_code: newJobCode,
+            job_name: newJobName,
+            price: newPrice,
+            isHourly: newIsHourly,
+            applies: newAppliesSelection.map(m => m.value).join(','),
+            isAsset: newIsAsset,
+            hourly_goal: newHourlyGoal,
+            statusOnly: newStatusOnly,
+            restricted_comments: newRestrictedComments
+        }
+        setLoading(true)
+        let token = await getTokenSilently()
+        let res = await jobService.add(formData, token)
+        if (res.isErrored) {
+            setLoading(false)
+            if (document.getElementById('new-jobcode')) document.getElementById('new-jobcode').classList.add('invalid')
+            if (document.getElementById('new-jobname')) document.getElementById('new-jobname').classList.add('invalid')
+            if (document.getElementById('new-price')) document.getElementById('new-price').classList.add('invalid')
+        } else {
+            if (document.getElementById('new-jobcode')) document.getElementById('new-jobcode').value = ''
+            if (document.getElementById('new-jobname')) document.getElementById('new-jobname').value = ''
+            if (document.getElementById('new-price')) document.getElementById('new-price').value = ''
+            setNewPrice(0)
+            setNewJobName('')
+            setNewJobCode('')
+            const response = await fetch(`${APILink}/all`, {
+                mode: 'cors',
+                headers: {
+                    'Authorization': `Bearer ${token}`,
+                    'Access-Control-Allow-Origin': '*',
+                    'X-Version': require('../backendVersion.json').version
+                }
+            });
+            const d = await response.json();
+            setData(d);
+            setLoading(false)
         }
     }
 
@@ -386,9 +353,11 @@ function JobPage(props) {
                         </tr>
                     </thead>
                     <tbody>
-                        {data.job_codes ? data.job_codes.map(m => RenderRow(m)) : <></>}
                         <tr>
-                            <td><input type='text' placeholder='T-Seets Name' className='job_code' id={`new-jobcode`} onBlur={(e) => handleTextInputChange('new', e)} onKeyDown={e => handleKeyDown('new', e)}></input></td>
+                            <td style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
+                                {newJobCode && newJobName && newPrice ? <i className="material-icons delete-icon" onClick={e => sendNewToAPI()}>save</i> : undefined}
+                                <input type='text' placeholder='T-Seets Name' className='job_code' id={`new-jobcode`} onBlur={(e) => handleTextInputChange('new', e)} onKeyDown={e => handleKeyDown('new', e)}></input>
+                            </td>
                             <td><input type='text' placeholder='Proper Name' className='job_name' id={`new-jobname`} onBlur={(e) => handleTextInputChange('new', e)} onKeyDown={e => handleKeyDown('new', e)}></input></td>
                             {props.permissions.view_reports || props.isAdmin ?
                                 <td><input type='number' placeholder='0' className='price' id={`new-price`} onBlur={(e) => { numberValidatorEventListener(e); handleTextInputChange('new', e) }} onKeyDown={e => { handleKeyDown('new', e) }} style={{ width: '5rem', padding: '1rem' }} step='0.01'></input></td>
@@ -424,6 +393,7 @@ function JobPage(props) {
                                 <input type='text' placeholder='Comma Seperated Comments' className='comments' id={`new-comments`} onBlur={(e) => handleTextInputChange('new', e)} onKeyDown={e => handleKeyDown('new', e)} />
                             </td>
                         </tr>
+                        {data.job_codes ? data.job_codes.map(m => RenderRow(m)) : <></>}
                     </tbody>
                 </table>
             </div>
