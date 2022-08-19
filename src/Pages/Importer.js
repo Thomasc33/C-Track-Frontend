f7bab840338dc0b4c23b525c0a66a6a817092e68
Thomas Carr <35206339+Tcarr9442@users.noreply.github.com>
2022-08-19 11:17:16 -0400

    Part System Ready

--- src/Pages/Importer.js
+++ src/Pages/Importer.js
@@ -33,6 +33,7 @@ function ImporterPage(props) {
             else if (importerType === 1) csv.push({ id: t[0], name: t[1], device_type: t[2], manufacturer: t[3] })
             else if (importerType === 2) csv.push({ id: t[0] })
             else if (importerType === 3) csv.push({ id: t[0] })
+            else if (importerType === 4) csv.push({ id: t[0], part_type: t[1], minimum_stock: t[2], models: t[3] })
         }
         confirm(csv)
     }
@@ -44,6 +45,7 @@ function ImporterPage(props) {
         else if (importerType === 1) for (let i of data) csv.push({ id: i[0], name: i[1], device_type: i[2], manufacturer: i[3] })
         else if (importerType === 2) for (let i of data) csv.push({ id: i[0] })
         else if (importerType === 3) for (let i of data) csv.push({ id: i[0] })
+        else if (importerType === 4) for (let i of data) csv.push({ id: i[0], part_type: i[1], minimum_stock: i[2], models: i[3] })
         confirm(csv)
     }
 
@@ -61,7 +63,8 @@ function ImporterPage(props) {
                             columns={importerType === 0 ? asset_columns :
                                 importerType === 1 ? model_columns :
                                     importerType === 2 ? legal_hold_columns :
-                                        importerType === 3 ? parts_columns : undefined}
+                                        importerType === 3 ? parts_columns :
+                                            importerType === 4 ? part_types_columns : undefined}
                             rows={data}
                             disableSelectionOnClick
                             hideFooterSelectedRowCount
@@ -101,7 +104,7 @@ function ImporterPage(props) {
             }
         }
         async function req(section) {
-            let res = await axios.post(`${settings.APIBase}/importer/${importerType === 0 ? 'asset' : importerType === 1 ? 'model' : importerType === 2 ? 'legal' : importerType === 3 ? 'parts' : undefined}`, section, {
+            let res = await axios.post(`${settings.APIBase}/importer/${importerType === 0 ? 'asset' : importerType === 1 ? 'model' : importerType === 2 ? 'legal' : importerType === 3 ? 'parts' : importerType === 4 ? 'part_types' : undefined}`, section, {
                 headers: {
                     'Access-Control-Allow-Origin': '*',
                     'Authorization': `Bearer ${token}`,
@@ -126,16 +129,18 @@ function ImporterPage(props) {
                 <h1>CSV Format - {importerType === 0 ? 'Assets' :
                     importerType === 1 ? 'Models' :
                         importerType === 2 ? 'Legal Hold' :
-                            importerType === 3 ? 'Parts'
-                                : undefined}</h1>
+                            importerType === 3 ? 'Parts' :
+                                importerType === 4 ? 'Part Types' : undefined}</h1>
                 <div style={{ display: 'inline-block', padding: '1rem' }}><h3 style={{ boxShadow: '0px 8px 16px 0px rgba(0, 0, 0, 0.2)', padding: '1rem', backgroundColor: '#1b1b1b', borderRadius: '.5rem', fontFamily: 'Consolas, monaco, monospace' }}>
                     {importerType === 0 ? asset_columns.map(m => m.field).join(',') :
                         importerType === 1 ? model_columns.map(m => m.field).join(',') :
                             importerType === 2 ? legal_hold_columns.map(m => m.field).join(',') :
-                                importerType === 3 ? parts_columns.map(m => m.field).join(',') : undefined}
+                                importerType === 3 ? parts_columns.map(m => m.field).join(',') :
+                                    importerType === 4 ? part_types_columns.map(m => m.field).join(',') : undefined}
                 </h3></div>
                 <br />
                 <h3>Do not include header row</h3>
+                {importerType === 4 ? <h3>Seperate Models with "/" and only use space if it is in the model number</h3> : undefined}
                 <hr style={{ marginTop: '2rem' }} />
                 <h3 style={{ fontSize: '1.5rem', fontWeight: 100 }}>Select CSV File with Information</h3>
                 <CSVReader cssClass="react-csv-input" onFileLoaded={handleData} />
@@ -177,6 +182,14 @@ function ImporterPage(props) {
                     onClick={e => setImporterType(2)}>
                     Legal Hold Importer
                 </Button>}
+                {importerType === 4 ? undefined : <Button
+                    variant='contained'
+                    color='primary'
+                    size='large'
+                    style={{ boxShadow: '0px 8px 16px 0px rgba(0, 0, 0, 0.2)', backgroundColor: localStorage.getItem('accentColor') || '#003994', marginLeft: '1rem', marginRight: '1rem' }}
+                    onClick={e => setImporterType(4)}>
+                    Part Type Importer
+                </Button>}
                 <br />
                 <br />
                 <br />
@@ -205,5 +218,12 @@ const legal_hold_columns = [
 ]
 
 const parts_columns = [
-    { field: 'part_number', headerName: 'Part Number', width: 400, editable: true }
+    { field: 'id', headerName: 'Part Number', width: 400, editable: true }
+]
+
+const part_types_columns = [
+    { field: 'id', headerName: 'Part Number', width: 250, editable: true },
+    { field: 'part_type', headerName: 'Part Type', width: 250, editable: true },
+    { field: 'minimum_stock', headerName: 'Minimum Stock', width: 200, editable: true },
+    { field: 'models', headerName: 'Model(s) - \'/\' Seperated', width: 400, editable: true }
 ]
--- src/Pages/Part Inventory.js
+++ src/Pages/Part Inventory.js
@@ -21,7 +21,7 @@ function PartInventoryPage(props) {
                 headers: { Authorization: `Bearer ${token}`, 'Access-Control-Allow-Origin': '*', 'X-Version': require('../backendVersion.json').version }
             })
             if (res.isErrored) return console.log(res)
-            setData(res.data || [])
+            if (res.data) setData(res.data.filter(part => !!part.model))
         }
         if (token) getData()
         // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -49,7 +49,7 @@ function PartInventoryPage(props) {
 
     function RenderModel() {
         let selectedInfo
-        for (let i of data) if (i.model.model_number === selectedModel) { selectedInfo = i; break }
+        for (let i of data) if (i.model && i.model.model_number === selectedModel) { selectedInfo = i; break }
         return <>
             <div style={{ display: 'flex', justifyContent: 'space-between', alignContent: 'center', width: '98%' }}>
                 <Button variant='contained' color='primary' size='large' style={{ boxShadow: 'box-shadow: 0 0 25px rgba(0, 0, 0, .1), 0 5px 10px -3px rgba(0, 0, 0, .13)', padding: '.5rem', margin: '.5rem', backgroundColor: localStorage.getItem('accentColor') || '#003994' }} onClick={() => { setSelectedModel(null) }}>Back</Button>
@@ -68,7 +68,7 @@ function PartInventoryPage(props) {
 
     function RenderPart() {
         let selectedInfo
-        for (let i of data) if (i.model.model_number === selectedModel) { selectedInfo = i; break }
+        for (let i of data) if (i.model && i.model.model_number === selectedModel) { selectedInfo = i; break }
         return <>
             <div style={{ display: 'flex', justifyContent: 'space-between', alignContent: 'center', width: '98%' }}>
                 <Button variant='contained' color='primary' size='large' style={{ boxShadow: 'box-shadow: 0 0 25px rgba(0, 0, 0, .1), 0 5px 10px -3px rgba(0, 0, 0, .13)', padding: '.5rem', margin: '.5rem', backgroundColor: localStorage.getItem('accentColor') || '#003994' }} onClick={() => { setSelectedPart(null) }}>Back</Button>
